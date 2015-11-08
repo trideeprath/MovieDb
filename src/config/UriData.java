7f75f3b28927cc74f993dc43662e4fe1b33533d5
Trideep Rath <trideeprath1991@gmail.com>
2015-11-07 20:22:13 -0700

    Config Created

---
+++ src/config/UriData.java
@@ -0,0 +1,31 @@
+package config;
+
+public class UriData {
+	
+	public static String getSingleFileUri(){
+		String URIString = "C://Trideep//Classes//NLP//IMDBMovieSearchData//synopses//departed_coref";
+		return URIString;
+	}
+	
+	public static String getCleanedPlotsFolderUri(){
+		String URIString = "C://Trideep//Classes//NLP//IMDBMovieSearchData//synopses//departed_coref";
+		return URIString;
+	}
+	
+	public static String getRawPlotsFolderUri(){
+		String URIString = "C://Trideep//Classes//NLP//IMDBMovieSearchData//synopses//departed_coref";
+		return URIString;
+	}
+	
+	public static String getCoreferencedPlotsFolderUri(){
+		String URIString = "C://Trideep//Classes//NLP//IMDBMovieSearchData//synopses//departed_coref";
+		return URIString;
+	}
+	
+	public static String getExtractedEventsFolderUri(){
+		String URIString = "C://Trideep//Classes//NLP//IMDBMovieSearchData//synopses//departed_coref";
+		return URIString;
+	}
+	
+
+}
